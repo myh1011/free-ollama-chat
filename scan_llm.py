d8270432bf19e432c7b075833ce59b276b125493
myh1011 <yma98591@gmail.com>
2025-03-23 09:06:57 +0800

    config

--- scan_llm.py
+++ scan_llm.py
@@ -2,7 +2,10 @@
 import requests
 import concurrent.futures
 
+#config
 TIMEOUT = 2
+work_thread = 20
+
 
 def fetch_ollama_models(ip_port):
     try:
@@ -17,7 +20,7 @@ def fetch_ollama_models(ip_port):
 
 def scan_ips(ip_port_list):
     result = {}
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=work_thread) as executor:
         futures = [executor.submit(fetch_ollama_models, ip) for ip in ip_port_list]
         for future in concurrent.futures.as_completed(futures):
             ip_port, models = future.result()
